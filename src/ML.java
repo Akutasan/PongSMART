7fb7d4448a8c2591d9948a63f69ffcdd3f2ed596
Akutasan <mirodurana1@gmail.com>
2023-12-06 21:22:13 +0100

    Commit commit

--- src/ML.java
+++ src/ML.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class ML {
     public static double distance = 0;
 
@@ -15,4 +18,43 @@ public class ML {
             return false;
         }
     }
+
+    public BufferedImage preprocessImage(BufferedImage image) {
+        BufferedImage grayImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+        Graphics g = grayImage.getGraphics();
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+
+        Image tmp = grayImage.getScaledInstance(80, 80, Image.SCALE_SMOOTH);
+        BufferedImage downscaledImage = new BufferedImage(80, 80, BufferedImage.TYPE_INT_ARGB);
+        g = downscaledImage.getGraphics();
+        g.drawImage(tmp, 0, 0, null);
+        g.dispose();
+
+        return downscaledImage;
+    }
+
+    public MultiLayerNetwork createNetwork() {
+        int numInputs = 6400;  // 80*80 input pixels
+        int numOutputs = 1;  // Up or down action
+        int numHiddenNodes = 200;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .updater(new Adam())
+                .weightInit(WeightInit.XAVIER)
+                .list()
+                .layer(new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation(Activation.SOFTMAX)
+                        .nIn(numHiddenNodes).nOut(numOutputs).build())
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        return model;
+    }
+
+
 }
--- src/QLearning.java
+++ src/QLearning.java
@@ -3,31 +3,31 @@ import java.util.Random;
 
 public class QLearning {
 
-    private final double alpha = 0.1; // Learning rate
-    private final double gamma = 0.9; // Eagerness - 0 looks in the near future, 1 looks in the distant future
-
-    private final int reward = 100;
-    private final int penalty = -10;
-
     private static int statesCount = 0;
-
     private static int y_dist;
     private static int x_pos_balls;
     private static int y_pos_racket;
     private static int true_y_pos_racket;
-
-    private int[][] R;       // Reward lookup
-    private double[][] Q;    // Q learning
+    private static int y_paddle_offset;
+    private static Ball ball;
+    private static int currentPlayer = 1;
+    private final double alpha = 0.1; // Learning rate
+    private final double gamma = 0.9; // Eagerness - 0 looks in the near future, 1 looks in the distant future
+    private final int reward = 1000;
+    private final int penalty = -10;
+    private int[][][] R;       // Reward lookup
+    private double[][][] Q;    // Q learning
 
 
     public static void main(String[] args) {
         QLearning ql = new QLearning();
         Pong activeGame = new Pong();
 
-        y_dist = (activeGame.getPanel().getBall().getY() - activeGame.getPanel().getPlayer(1).getY());
+        y_dist = (activeGame.getPanel().getBall().getY() - activeGame.getPanel().getPlayer(currentPlayer).getY());
         x_pos_balls = activeGame.getPanel().getBall().getX();
-        y_pos_racket = activeGame.getPanel().getPlayer(1).getY();
-        true_y_pos_racket = y_pos_racket + activeGame.getPanel().getPlayer(1).getBounds().height / 2;
+        y_pos_racket = activeGame.getPanel().getPlayer(currentPlayer).getY();
+        true_y_pos_racket = activeGame.getPanel().getPlayer(currentPlayer).getBounds().height / 2;
+        ball = activeGame.getPanel().getBall();
         statesCount = Pong.WIDTH * Pong.HEIGHT;
 
         ql.init();
@@ -36,10 +36,10 @@ public class QLearning {
         ql.printPolicy();
     }
 
-    public void init() {
+    public void init(PongPanel game) {
 
-        R = new int[statesCount][statesCount];
-        Q = new double[statesCount][statesCount];
+        R = new int[statesCount][statesCount][statesCount];
+        Q = new double[statesCount][statesCount][statesCount];
 
         // We will navigate through the reward matrix R using k index
         for (int k = 0; k < statesCount; k++) {
@@ -48,7 +48,9 @@ public class QLearning {
 
             // Fill in the reward matrix with -1
             for (int s = 0; s < statesCount; s++) {
-                R[k][s] = -1;
+                for (int a = 0; a < statesCount; a++) {
+                    R[s][a][k] = -1;
+                }
             }
 
             // TODO: Reward 0 when ball does not hit racket
@@ -58,7 +60,22 @@ public class QLearning {
             // Paddle doesn't go OOB
 
             // Paddle Down
-            if (
+            if (y_pos_racket - true_y_pos_racket > Pong.HEIGHT * -1) { // Can it go down?
+                if (ball.checkCollision() == game.getPlayer(currentPlayer) || game.getBall().getX() < 0) { // Is ball hitting racket or opposite wall
+                    R[k][y_pos_racket][x_pos_balls] = reward;
+                } else {
+                    R[k][y_pos_racket][x_pos_balls] = penalty;
+                }
+            }
+
+            // Paddle Up
+            if (y_pos_racket + true_y_pos_racket < Pong.HEIGHT) { // Can it go up?
+                if (ball.checkCollision() == game.getPlayer(currentPlayer) || game.getBall().getX() < 0) { // Is ball hitting racket or opposite wall
+                    R[k][y_pos_racket][x_pos_balls] = reward;
+                } else {
+                    R[k][y_pos_racket][x_pos_balls] = penalty;
+                }
+            }
 
             // Try to move down in the maze
 //                int goDown = i + 1;
@@ -82,13 +99,15 @@ public class QLearning {
     void initializeQ() {
         for (int i = 0; i < statesCount; i++) {
             for (int j = 0; j < statesCount; j++) {
-                Q[i][j] = (double) R[i][j];
+                for (int k = 0; k < statesCount; k++) {
+                    Q[i][j][k] = R[i][j][k];
+                }
             }
         }
     }
 
     // Used for debug
-    void printR(int[][] matrix) {
+    void printR(int[][][] matrix) {
         System.out.printf("%25s", "States: ");
         for (int i = 0; i <= 8; i++) {
             System.out.printf("%4s", i);
@@ -119,12 +138,12 @@ public class QLearning {
                 int nextState = actionsFromCurrentState[index];
 
                 // Q(state,action)= Q(state,action) + alpha * (R(state,action) + gamma * Max(next state, all actions) - Q(state,action))
-                double q = Q[crtState][nextState];
+                double q = Q[crtState][nextState][];
                 double maxQ = maxQ(nextState);
-                int r = R[crtState][nextState];
+                int r = R[crtState][nextState][];
 
                 double value = q + alpha * (r + gamma * maxQ - q);
-                Q[crtState][nextState] = value;
+                Q[crtState][nextState] = new double[]{value};
 
                 crtState = nextState;
             }
@@ -141,8 +160,10 @@ public class QLearning {
     int[] possibleActionsFromState(int state) {
         ArrayList<Integer> result = new ArrayList<>();
         for (int i = 0; i < statesCount; i++) {
-            if (R[state][i] != -1) {
-                result.add(i);
+            for (int j = 0; j < statesCount; j++) {
+                if (R[state][i][j] != -1) {
+                    result.add(i);
+                }
             }
         }
 
@@ -154,7 +175,7 @@ public class QLearning {
         //the learning rate and eagerness will keep the W value above the lowest reward
         double maxValue = -10;
         for (int nextAction : actionsFromState) {
-            double value = Q[nextState][nextAction];
+            double value = Q[nextState][nextAction][];
 
             if (value > maxValue)
                 maxValue = value;
